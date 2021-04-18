521bddc31a85d66d0f05de4985e4279b0b339db3
Simon Walker <s.r.walker101@googlemail.com>
2021-04-18 02:16:48 +0100

    Updates database layout

--- mealplanner/models.py
+++ mealplanner/models.py
@@ -13,7 +13,6 @@ class Ingredient(db.Model):
     protein = db.Column(db.Float)
 
     def to_dict(self):
-        print(uuid.uuid4())
         return {
             "id": str(self.id),
             "name": self.name,
@@ -21,4 +20,48 @@ class Ingredient(db.Model):
             "saturated_fat": self.saturated_fat,
             "carbohydrate": self.carbohydrate,
             "protein": self.protein,
+        }
+
+
+class Recipe(db.Model):
+    id = db.Column(UUID(), primary_key=True, default=uuid.uuid4, unique=True)
+    name = db.Column(db.String, nullable=False, unique=True)
+
+    def to_dict(self):
+        return {
+            "id": str(self.id),
+            "name": self.name,
+            "membership": self.membership,
+        }
+
+
+class Membership(db.Model):
+    id = db.Column(UUID(), primary_key=True, default=uuid.uuid4, unique=True)
+    ingredient_id = db.Column(UUID, db.ForeignKey("ingredient.id", ondelete="CASCADE"))
+    ingredient = db.relationship("Ingredient", backref="memberships")
+    recipe_id = db.Column(UUID, db.ForeignKey("recipe.id", ondelete="CASCADE"))
+    recipes = db.relationship("Recipe", backref="memberships")
+
+    def to_dict(self):
+        return {
+            "id": str(self.id),
+            "recipe": self.recipe.to_dict(),
+        }
+
+
+class Allocation(db.Model):
+    id = db.Column(UUID(), primary_key=True, default=uuid.uuid4, unique=True)
+    meal = db.Column(db.String, nullable=False)
+    day = db.Column(db.String, nullable=False)
+    membership_id = db.Column(UUID, db.ForeignKey("membership.id", ondelete="CASCADE"))
+    membership = db.relationship("Membership", backref="allocations")
+    # recipe_id = db.Column(UUID, db.ForeignKey("recipe.id", ondelete="CASCADE"))
+    # recipe = db.relationship("Recipe", back_populates="allocations")
+
+    def to_dict(self):
+        return {
+            "id": str(self.id),
+            "meal": self.meal,
+            "day": self.day,
+            "recipe": self.recipe.to_dict(),
         }
